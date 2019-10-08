661153493223cfc41d6737e3d85026168da6ef34
Patrick Untiet <p.u@wwu.de>
2019-10-08 14:43:35 +0200

    add dimensions and point data classes

---
+++ src/misc.py
@@ -0,0 +1,15 @@
+class dimensions:
+    """
+    Width/Height pair.
+    """
+    def __init__():
+        self.w = 0
+        self.h = 0
+        
+class point:
+    """
+    X/Y coordinate.
+    """
+    def __init__():
+        self.x = 0
+        self.y = 0
