85fb8c4d342cd0ee6eb53d96bf4cc69ee9031b16
Patrick Untiet <p.u@wwu.de>
2019-10-08 16:17:57 +0200

    add missing self parameters from inits (whoops)

--- src/misc.py
+++ src/misc.py
@@ -2,7 +2,7 @@ class dimensions:
     """
     Width/Height pair.
     """
-    def __init__():
+    def __init__(self):
         self.w = 0
         self.h = 0
         
@@ -10,7 +10,7 @@ class point:
     """
     X/Y coordinate.
     """
-    def __init__():
+    def __init__(self):
         self.x = 0
         self.y = 0
         
--- src/room_elements.py
+++ src/room_elements.py
@@ -9,7 +9,7 @@ class element_type(Enum):
     other = 3
     
 class room_element:
-    def __init__():
+    def __init__(self):
         self.dims = dimensions()
         self.type = element_type.other
         self.xp_value = 0
@@ -19,14 +19,14 @@ class room_element:
         return self.dims.w * self.dims.h
     
 class enemy(room_element):
-    def __init__():
+    def __init__(self):
         self.type = element_type.enemy
         
 class trap(room_element):
-    def __init__():
+    def __init__(self):
         self.type = element_type.trap
         
 class treasure(room_element):
-    def __init__():
+    def __init__(self):
         self.type = element_type.treasure
         self.gp_value = 0
